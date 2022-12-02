9faecc452e77e9a6f15efca72c601f7f37af48c8
shubham_unifyed <shubham_gupta@unifyed.com>
2022-12-02 19:08:41 +0530

    file added

--- unifyed-setup-product/src/components/shared/layout/Layout.js
+++ unifyed-setup-product/src/components/shared/layout/Layout.js
@@ -1,43 +1,16 @@
 import Header from "components/shared/header/Header";
 import { SideBar } from "components/shared/sidebar/SideBar";
 import { AuthState } from "context/AuthContext";
-import { memo, useEffect, useState } from "react";
+import { memo } from "react";
 import { withRouter } from "react-router";
 
 const Layout = ({ children, location, history }) => {
-  let pathname = location.pathname;
-  const [breadcrumbPathname, setBreadcrumbPathname] = useState(pathname);
-  const setBreadcrumb = (pathname) => {
-    const formPaths = ["/edit/", "/view/"];
-    let bcPath = pathname;
-    const formPathInBreadcrumb = formPaths.filter((path) =>
-      bcPath.includes(path)
-    );
-    if (formPathInBreadcrumb.length > 0) {
-      bcPath = bcPath.substring(0, bcPath.lastIndexOf("/"));
-    }
-    if (bcPath !== "/" && bcPath.endsWith("/")) {
-      bcPath = bcPath.substring(0, bcPath.lastIndexOf("/"));
-    }
-    setBreadcrumbPathname(bcPath);
-  };
-
-  useEffect(() => {
-    const unlisten = history.listen((location) => {
-      // location is an object like window.location
-      setBreadcrumb(location.pathname);
-    });
-    return () => {
-      unlisten();
-    };
-  }, []);
-
   return (
     <div className="min-h-screen md:flex flex-row bg-gray-100 overflow-x-hidden overflow-y-hidden">
       {AuthState.user ? <SideBar /> : <SideBar />}
 
       <div className="flex-1">
-        <Header children={children} breadcrumbPathname={breadcrumbPathname} />
+        <Header children={children} />
       </div>
     </div>
   );
