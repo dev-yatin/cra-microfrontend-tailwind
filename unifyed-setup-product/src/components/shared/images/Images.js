f46b6da9ff019d2591a7c8b5605e47533f041155
Ravi Kumar <Ravi_kumar@blackbelthelp.com>
2022-12-06 18:18:16 +0530

    #removed consolg log

--- unifyed-setup-product/src/components/shared/images/Images.js
+++ unifyed-setup-product/src/components/shared/images/Images.js
@@ -6,7 +6,6 @@
 import NoImg from 'assets/no-photo.png';
 
 const Images = (props) => {
-    console.log("images" + props)
     return (
         <img src={props.src ? props.src : NoImg} alt={props.alt ? props.alt : ""} width={props.width ? props.width : ''} height={props.height ? props.height : ""} style={props.style ? props.style : ""} />
     )
